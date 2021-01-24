ac587e800a4e67a4f9bece9f068025f6cae07a6e
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2021-01-24 01:27:19 +0100

    refactor: renamed all file names

--- src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
+++ src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package nl.michelbijnen.minecraft.alien.science;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class AlienScience implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
