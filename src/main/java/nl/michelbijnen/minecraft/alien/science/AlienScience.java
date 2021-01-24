2dcda91117575fcca615a0c9df85a10e3c0c3bca
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2021-01-24 15:39:42 +0100

    feat: added random structures

--- src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
+++ src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
@@ -1,7 +1,10 @@
 package nl.michelbijnen.minecraft.alien.science;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -9,8 +12,15 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.structure.StructurePieceType;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.ConfiguredStructureFeature;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
 
 public class AlienScience implements ModInitializer {
 
@@ -22,6 +32,10 @@ public class AlienScience implements ModInitializer {
 
 	public static final Block MOB_TESTER = new Block(FabricBlockSettings.of(Material.STONE).strength(1F, 6F).sounds(BlockSoundGroup.GLASS).breakByTool(FabricToolTags.PICKAXES).requiresTool());
 
+	public static final StructurePieceType TESTING_TENT_PIECE = TestingTentGenerator.TestingTentPiece::new;
+	private static final StructureFeature<DefaultFeatureConfig> TESTING_TENT_STRUCTURE = new TestingTentFeature(DefaultFeatureConfig.CODEC);
+	private static final ConfiguredStructureFeature<?, ?> TESTING_TENT_CONFIGURED = TESTING_TENT_STRUCTURE.configure(DefaultFeatureConfig.DEFAULT);
+
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("alienscience", "alien_finder"), ALIEN_FINDER);
@@ -30,5 +44,18 @@ public class AlienScience implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("alienscience", "edible_alien_finder"), EDIBLE_ALIEN_FINDER);
 		Registry.register(Registry.BLOCK, new Identifier("alienscience", "mob_tester"), MOB_TESTER);
 		Registry.register(Registry.ITEM, new Identifier("alienscience", "mob_tester"), new BlockItem(MOB_TESTER, new Item.Settings().group(ItemGroup.REDSTONE)));
+
+		Registry.register(Registry.STRUCTURE_PIECE, new Identifier("alienscience", "testing_tent_piece"), TESTING_TENT_PIECE);
+		FabricStructureBuilder.create(new Identifier("alienscience", "testing_tent"), TESTING_TENT_STRUCTURE)
+				.step(GenerationStep.Feature.SURFACE_STRUCTURES)
+				.defaultConfig(32, 8, 12345)
+				.adjustsSurface()
+				.register();
+
+		RegistryKey<ConfiguredStructureFeature<?, ?>> myConfigured = RegistryKey.of(Registry.CONFIGURED_STRUCTURE_FEATURE_WORLDGEN,
+				new Identifier("alienscience", "testing_tent"));
+		BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_STRUCTURE_FEATURE, myConfigured.getValue(), TESTING_TENT_CONFIGURED);
+
+		BiomeModifications.addStructure(BiomeSelectors.foundInOverworld(), myConfigured);
 	}
 }
---
+++ src/main/java/nl/michelbijnen/minecraft/alien/science/TestingTentGenerator.java
@@ -0,0 +1,64 @@
+package nl.michelbijnen.minecraft.alien.science;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.structure.*;
+import net.minecraft.structure.processor.BlockIgnoreStructureProcessor;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ServerWorldAccess;
+
+import java.util.List;
+import java.util.Random;
+
+public class TestingTentGenerator {
+    private static final Identifier TESTING_TENT = new Identifier("alienscience", "testingtent/testingtent");
+
+    public static void addPieces(StructureManager manager, BlockPos pos, BlockRotation rotation, List<StructurePiece> pieces) {
+        pieces.add(new TestingTentPiece(manager, pos, TESTING_TENT, rotation));
+    }
+
+    public static class TestingTentPiece extends SimpleStructurePiece {
+        private final BlockRotation rotation;
+        private final Identifier template;
+
+        public TestingTentPiece(StructureManager structureManager, CompoundTag compoundTag) {
+            super(AlienScience.TESTING_TENT_PIECE, compoundTag);
+            this.template = new Identifier(compoundTag.getString("Template"));
+            this.rotation = BlockRotation.valueOf(compoundTag.getString("Rot"));
+            this.initializeStructureData(structureManager);
+        }
+
+        public TestingTentPiece(StructureManager structureManager, BlockPos pos, Identifier template, BlockRotation rotation) {
+            super(AlienScience.TESTING_TENT_PIECE, 0);
+            this.pos = pos;
+            this.rotation = rotation;
+            this.template = template;
+
+            this.initializeStructureData(structureManager);
+        }
+
+        private void initializeStructureData(StructureManager structureManager) {
+            Structure structure = structureManager.getStructureOrBlank(this.template);
+            StructurePlacementData placementData = (new StructurePlacementData())
+                    .setRotation(this.rotation)
+                    .setMirror(BlockMirror.NONE)
+                    .addProcessor(BlockIgnoreStructureProcessor.IGNORE_STRUCTURE_BLOCKS);
+            this.setStructureData(structure, this.pos, placementData);
+        }
+
+        protected void toNbt(CompoundTag tag) {
+            super.toNbt(tag);
+            tag.putString("Template", this.template.toString());
+            tag.putString("Rot", this.rotation.name());
+        }
+
+        @Override
+        protected void handleMetadata(String metadata, BlockPos pos, ServerWorldAccess serverWorldAccess, Random random,
+                                      BlockBox boundingBox) {
+        }
+    }
+
+}
