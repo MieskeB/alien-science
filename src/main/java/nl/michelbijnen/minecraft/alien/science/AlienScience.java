af18eac275f9c326ec07f6c0563e57bf39f98e23
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2021-01-24 01:38:22 +0100

    feat: added alien finder

--- src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
+++ src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
@@ -1,14 +1,17 @@
 package nl.michelbijnen.minecraft.alien.science;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class AlienScience implements ModInitializer {
+
+	public static final Item ALIEN_FINDER = new Item(new Item.Settings().group(ItemGroup.REDSTONE));
+
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("alienscience", "alien_finder"), ALIEN_FINDER);
 	}
 }
