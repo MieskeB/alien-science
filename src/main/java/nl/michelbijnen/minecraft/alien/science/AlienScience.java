f2f6519c4870831efdf28b89485950c726ee64ac
Michel Bijnen <michelbijnenmichelbijnen@gmail.com>
2021-01-24 02:21:04 +0100

    feat: added moon stone

--- src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
+++ src/main/java/nl/michelbijnen/minecraft/alien/science/AlienScience.java
@@ -1,8 +1,14 @@
 package nl.michelbijnen.minecraft.alien.science;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -10,8 +16,12 @@ public class AlienScience implements ModInitializer {
 
 	public static final Item ALIEN_FINDER = new Item(new Item.Settings().group(ItemGroup.REDSTONE));
 
+	public static final Block MOON_STONE = new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6F).sounds(BlockSoundGroup.STONE).breakByTool(FabricToolTags.PICKAXES));
+
 	@Override
 	public void onInitialize() {
 		Registry.register(Registry.ITEM, new Identifier("alienscience", "alien_finder"), ALIEN_FINDER);
+		Registry.register(Registry.BLOCK, new Identifier("alienscience", "moon_stone"), MOON_STONE);
+		Registry.register(Registry.ITEM, new Identifier("alienscience", "moon_stone"), new BlockItem(MOON_STONE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 	}
 }
